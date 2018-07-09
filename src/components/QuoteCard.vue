b73612c75105db921decb8ff492be296ad630c76
Berkan Cetinkaya <berkan.cetinkaya@windowslive.com>
2018-07-09 15:43:18 +0300

    feat: add quote card ui

--- src/components/QuoteCard.vue
+++ src/components/QuoteCard.vue
@@ -1,22 +1,51 @@
 <template>
-    <p>Hey from Quote Card</p>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="card">
+          <div class="card-content">
+            <p class="title">
+              “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
+            </p>
+            <p class="subtitle">
+              Jeff Atwood
+            </p>
+          </div>
+          <footer class="card-footer">
+            <p class="card-footer-item">
+                  <button class="button is-link is-size-5">Show another one</button>
+            </p>
+            <p class="card-footer-item">
+              <social-sharing url="https://localhost:3000/" title="Rumi Quote Machine" description="There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors." quote="There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors."
+              inline-template>
+                <div>
+                  <network network="facebook">
+                    <font-awesome-icon size="3x" color="#4267B2" :icon="['fab', 'facebook-square']" />
+                  </network>
+                  <network network="twitter">
+                    <font-awesome-icon size="3x" color="#1DA1F2" :icon="['fab', 'twitter-square']" />
+                  </network>
+                </div>
+              </social-sharing>
+            </p>
+          </footer>
+        </div>
+      </div>
+    </div>
+
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'QuoteCard',
-  data () {
-    return {
-
-    }
+  name: "QuoteCard",
+  data() {
+    return {};
   },
-  created() {
-
-  }
-}
+  created() {}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+import SocialSharing from 'vue-social-sharing'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebookSquare, faTwitterSquare } from '@fortawesome/fontawesome-free-brands';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faFacebookSquare, faTwitterSquare)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 
 Vue.config.productionTip = false
+
+
+Vue.use(Buefy)
+Vue.use(SocialSharing)
+
 
 /* eslint-disable no-new */
 new Vue({
